c7ddc39fb320cde7ee86f2384b3891b29cf4a1f9
yangqihua <904693433@qq.com>
2018-01-16 15:54:28 +0800

    环境搭好了

---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "http://yangqihua.com/agent/index.php/public/api/";
+export const USER_INFO = "userInfo";
+export const SYSTEM_INFO = "systemInfo";
+export const ADDRESS_ID = "addressId";
+export const SEL_CLASS_CODE = "selClassCode";
