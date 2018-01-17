51ea8f43fef046b8a980abea0070970a1f9de44d
yangqihua <904693433@qq.com>
2018-01-18 00:09:20 +0800

    大改之前提交一遍

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://192.168.1.105/agent/public/index.php/api/";
+// export const BASE_URL = "http://192.168.1.105/agent/public/index.php/api/";
+export const BASE_URL = "http://www.qu-gou.com/agent/public/index.php/api/";
 export const USER_INFO = "userInfo";
 export const SYSTEM_INFO = "systemInfo";
 export const ADDRESS_ID = "addressId";
