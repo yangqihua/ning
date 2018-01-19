2bba7542927b63b1b6d9fc20d2909895b7ee0b7e
yangqihua <904693433@qq.com>
2018-01-19 17:36:19 +0800

    优化

---
+++ src/mixins/common.js
@@ -0,0 +1,25 @@
+import wepy from 'wepy'
+
+export default class testMixin extends wepy.mixin {
+	data = {
+		scrollTop: 0
+	}
+	methods = {
+		goTop(e){
+			wx.pageScrollTo({scrollTop: 0})
+		}
+	}
+	computed = {
+		isAgent(){
+			return true;
+		}
+	}
+
+	onShow() {
+		// console.log('mixin onShow')
+	}
+
+	onLoad() {
+		// console.log('mixin onLoad')
+	}
+}
