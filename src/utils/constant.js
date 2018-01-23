9d9ec6034ec6252b88f3b73e7e226b1606c358bf
yangqihua <904693433@qq.com>
2018-01-23 22:49:43 +0800

    优化

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,6 @@
-export const BASE_URL = "http://192.168.1.105/agent/public/index.php/api/";
-export const PUBLIC = "http://192.168.1.105/agent/public/";
-// export const BASE_URL = "https://www.qu-gou.com/agent/public/index.php/api/";
+// const BASE = 'http://192.168.1.105/'
+const BASE = 'https://www.qu-gou.com/'
+export const BASE_URL = BASE+"agent/public/index.php/api/";
+export const PUBLIC = BASE+"agent/public/";
 export const USER_INFO = "userInfo";
 export const SYSTEM_INFO = "systemInfo";
-export const ADDRESS_ID = "addressId";
-export const SEL_CLASS_CODE = "selClassCode";
