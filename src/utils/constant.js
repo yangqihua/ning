da04b92854ff652bd30d09dd95575deb672c8511
yangqihua <904693433@qq.com>
2018-01-27 19:14:22 +0800

    修改一些文案

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
 // const BASE = 'http://192.168.1.105/'
-// const BASE = 'https://www.qu-gou.com/agent/'
-const BASE = 'https://www.qu-gou.com/agent2/'
+const BASE = 'https://www.qu-gou.com/agent/'
+// const BASE = 'https://www.qu-gou.com/agent2/'
 export const BASE_URL = BASE+"public/index.php/api/";
 export const PUBLIC = BASE+"public/";
 export const USER_INFO = "userInfo";
