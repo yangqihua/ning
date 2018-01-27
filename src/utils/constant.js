986de223904a2cce5276f4d2b219beda57d9ec23
yangqihua <904693433@qq.com>
2018-01-27 15:32:50 +0800

    我的用户界面修改

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
-const BASE = 'http://192.168.1.105/'
-// const BASE = 'https://www.qu-gou.com/'
+// const BASE = 'http://192.168.1.105/'
+const BASE = 'https://www.qu-gou.com/'
 export const BASE_URL = BASE+"agent/public/index.php/api/";
 export const PUBLIC = BASE+"agent/public/";
 export const USER_INFO = "userInfo";
