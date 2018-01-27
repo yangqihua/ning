82e98073b0d54c8000d270ecba7ebef180dc75e7
yangqihua <904693433@qq.com>
2018-01-27 16:52:28 +0800

    修改为 agent2 的接口

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,7 @@
 // const BASE = 'http://192.168.1.105/'
-const BASE = 'https://www.qu-gou.com/'
-export const BASE_URL = BASE+"agent/public/index.php/api/";
-export const PUBLIC = BASE+"agent/public/";
+// const BASE = 'https://www.qu-gou.com/agent/'
+const BASE = 'https://www.qu-gou.com/agent2/'
+export const BASE_URL = BASE+"public/index.php/api/";
+export const PUBLIC = BASE+"public/";
 export const USER_INFO = "userInfo";
 export const SYSTEM_INFO = "systemInfo";
